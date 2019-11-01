7c777b9524189a78bfc253abdc9d3decba28c36d
lucas10077 <lucaslima10077@gmail.com>
2019-11-01 14:47:01 -0300

    first commit

---
+++ ProjetoLoja/Projeto/CasaDoCodigo/Models/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+    // DataContract É  um contrato formal entre um serviço e um cliente que descreve abstratamente os dados a serem trocados.
+    // Ou seja, para se comunicar, o cliente e o serviço não precisam compartilhar os mesmos tipos, apenas os mesmos 
+    //contratos de dados.
+    [DataContract]
+    public abstract class BaseModel
+    {
+        [DataMember] //[DataMember] é aplicado ao membro de um tipo, especifica que o membro faz parte de um contrato de dados.
+        public int Id { get; protected set; }
+    }
+}
